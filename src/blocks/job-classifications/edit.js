f21bc92f62496774502c66ed8d318290d33c32a6
Adam Turner <adam.turner@wsu.edu>
2020-01-24 14:54:06 -0800

    No longer need to extend Component class

--- src/blocks/job-classifications/edit.js
+++ src/blocks/job-classifications/edit.js
@@ -3,7 +3,6 @@
  */
 const { __ } = wp.i18n;
 const { withSelect } = wp.data;
-const { Component } = wp.element;
 const { InspectorControls } = wp.blockEditor;
 const {
 	Disabled,
@@ -16,81 +15,74 @@ const {
 	ToggleControl,
 } = wp.components;
 
-class JobClassifications extends Component {
-	constructor() {
-		super( ...arguments );
-	}
+function JobClassifications( {
+	attributes,
+	setAttributes,
+	tables,
+} ) {
+	const {
+		className,
+		columns,
+		isStriped,
+		queryTable,
+	} = attributes;
 
-	render() {
-		const {
-			attributes,
-			setAttributes,
-			tables,
-		} = this.props;
-		const {
-			className,
-			columns,
-			isStriped,
-			queryTable,
-		} = attributes;
-
-		const inspectorControls = (
-			<InspectorControls>
-				{ 'is-style-list' !== className &&
-					<PanelBody title={ __( 'Table Settings' ) }>
-						<ToggleControl
-							label={ __( 'Striped table rows' ) }
-							checked={ isStriped }
-							onChange={ ( value ) => setAttributes( { isStriped: value } ) }
-						/>
-					</PanelBody>
-				}
-				{ 'is-style-list' === className &&
-					<PanelBody title={ __( 'List Settings' ) }>
-						<RangeControl
-							label={ __( 'List Columns' ) }
-							value={ columns || 3 }
-							onChange={ ( value ) => setAttributes( { columns: value } ) }
-							min={ 1 }
-							max={ 6 }
-						/>
-					</PanelBody>
-				}
-				<PanelBody title={ __( 'Select Job Data Source' ) }>
-					<SelectControl
-						className={ 'salary-data-table-picker__select' }
-						label={ __( 'Select desired group:' ) }
-						value={ queryTable }
-						options={ tables }
-						onChange={ ( value ) => setAttributes( { queryTable: value } ) }
+	const inspectorControls = (
+		<InspectorControls>
+			{ 'is-style-list' !== className &&
+				<PanelBody title={ __( 'Table Settings' ) }>
+					<ToggleControl
+						label={ __( 'Striped table rows' ) }
+						checked={ isStriped }
+						onChange={ ( value ) => setAttributes( { isStriped: value } ) }
 					/>
 				</PanelBody>
-			</InspectorControls>
-		);
+			}
+			{ 'is-style-list' === className &&
+				<PanelBody title={ __( 'List Settings' ) }>
+					<RangeControl
+						label={ __( 'List Columns' ) }
+						value={ columns || 3 }
+						onChange={ ( value ) => setAttributes( { columns: value } ) }
+						min={ 1 }
+						max={ 6 }
+					/>
+				</PanelBody>
+			}
+			<PanelBody title={ __( 'Select Job Data Source' ) }>
+				<SelectControl
+					className={ 'salary-data-table-picker__select' }
+					label={ __( 'Select desired group:' ) }
+					value={ queryTable }
+					options={ tables }
+					onChange={ ( value ) => setAttributes( { queryTable: value } ) }
+				/>
+			</PanelBody>
+		</InspectorControls>
+	);
 
-		if ( ! queryTable ) {
-			return (
-				<>
-					{ inspectorControls }
-					<Placeholder icon="admin-post" label={ __( 'Job Classification Data' ) }>
-						{ ! Array.isArray( tables ) ?
-							<Spinner /> :
-							__( 'Select a job classification data group to display results.' )
-						}
-					</Placeholder>
-				</>
-			);
-		}
-
+	if ( ! queryTable ) {
 		return (
 			<>
 				{ inspectorControls }
-				<Disabled>
-					<ServerSideRender block="hrswpsqlsrv/job-classifications" attributes={ attributes } />
-				</Disabled>
+				<Placeholder icon="admin-post" label={ __( 'Job Classification Data' ) }>
+					{ ! Array.isArray( tables ) ?
+						<Spinner /> :
+						__( 'Select a job classification data group to display results.' )
+					}
+				</Placeholder>
 			</>
 		);
 	}
+
+	return (
+		<>
+			{ inspectorControls }
+			<Disabled>
+				<ServerSideRender block="hrswpsqlsrv/job-classifications" attributes={ attributes } />
+			</Disabled>
+		</>
+	);
 }
 
 export default withSelect( ( select ) => {
--- src/blocks/salary-data/edit.js
+++ src/blocks/salary-data/edit.js
@@ -3,7 +3,6 @@
  */
 const { __ } = wp.i18n;
 const { withSelect } = wp.data;
-const { Component } = wp.element;
 const { InspectorControls } = wp.blockEditor;
 const {
 	Disabled,
@@ -16,81 +15,74 @@ const {
 	ToggleControl,
 } = wp.components;
 
-class SalaryData extends Component {
-	constructor() {
-		super( ...arguments );
-	}
+function SalaryData( {
+	attributes,
+	setAttributes,
+	tables,
+} ) {
+	const {
+		className,
+		columns,
+		isStriped,
+		queryTable,
+	} = attributes;
 
-	render() {
-		const {
-			attributes,
-			setAttributes,
-			tables,
-		} = this.props;
-		const {
-			className,
-			columns,
-			isStriped,
-			queryTable,
-		} = attributes;
-
-		const inspectorControls = (
-			<InspectorControls>
-				<PanelBody title={ __( 'Select Data Source' ) }>
-					<SelectControl
-						className={ 'salary-data-table-picker__select' }
-						label={ __( 'Select desired group:' ) }
-						value={ queryTable }
-						options={ tables }
-						onChange={ ( value ) => setAttributes( { queryTable: value } ) }
+	const inspectorControls = (
+		<InspectorControls>
+			<PanelBody title={ __( 'Select Data Source' ) }>
+				<SelectControl
+					className={ 'salary-data-table-picker__select' }
+					label={ __( 'Select desired group:' ) }
+					value={ queryTable }
+					options={ tables }
+					onChange={ ( value ) => setAttributes( { queryTable: value } ) }
+				/>
+			</PanelBody>
+			{ 'is-style-list' !== className &&
+				<PanelBody title={ __( 'Table Settings' ) }>
+					<ToggleControl
+						label={ __( 'Striped table rows' ) }
+						checked={ isStriped }
+						onChange={ ( value ) => setAttributes( { isStriped: value } ) }
 					/>
 				</PanelBody>
-				{ 'is-style-list' !== className &&
-					<PanelBody title={ __( 'Table Settings' ) }>
-						<ToggleControl
-							label={ __( 'Striped table rows' ) }
-							checked={ isStriped }
-							onChange={ ( value ) => setAttributes( { isStriped: value } ) }
-						/>
-					</PanelBody>
-				}
-				{ 'is-style-list' === className &&
-					<PanelBody title={ __( 'List Settings' ) }>
-						<RangeControl
-							label={ __( 'List Columns' ) }
-							value={ columns || 3 }
-							onChange={ ( value ) => setAttributes( { columns: value } ) }
-							min={ 1 }
-							max={ 6 }
-						/>
-					</PanelBody>
-				}
-			</InspectorControls>
-		);
+			}
+			{ 'is-style-list' === className &&
+				<PanelBody title={ __( 'List Settings' ) }>
+					<RangeControl
+						label={ __( 'List Columns' ) }
+						value={ columns || 3 }
+						onChange={ ( value ) => setAttributes( { columns: value } ) }
+						min={ 1 }
+						max={ 6 }
+					/>
+				</PanelBody>
+			}
+		</InspectorControls>
+	);
 
-		if ( ! queryTable ) {
-			return (
-				<>
-					{ inspectorControls }
-					<Placeholder icon="admin-post" label={ __( 'Salary Data' ) }>
-						{ ! Array.isArray( tables ) ?
-							<Spinner /> :
-							__( 'Select a salary data group to display results.' )
-						}
-					</Placeholder>
-				</>
-			);
-		}
-
+	if ( ! queryTable ) {
 		return (
 			<>
 				{ inspectorControls }
-				<Disabled>
-					<ServerSideRender block="hrswpsqlsrv/salary-data" attributes={ attributes } />
-				</Disabled>
+				<Placeholder icon="admin-post" label={ __( 'Salary Data' ) }>
+					{ ! Array.isArray( tables ) ?
+						<Spinner /> :
+						__( 'Select a salary data group to display results.' )
+					}
+				</Placeholder>
 			</>
 		);
 	}
+
+	return (
+		<>
+			{ inspectorControls }
+			<Disabled>
+				<ServerSideRender block="hrswpsqlsrv/salary-data" attributes={ attributes } />
+			</Disabled>
+		</>
+	);
 }
 
 export default withSelect( ( select ) => {
