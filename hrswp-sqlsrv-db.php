ed52b454098ae33f0b5bbf2b5c176a4c13d0cca2
Adam Turner <act@adamturner.dev>
2022-09-06 09:32:25 -0700

    Close #92 switch from setup class to functions

--- hrswp-sqlsrv-db.php
+++ hrswp-sqlsrv-db.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: HRSWP Sqlsrv DB
- * Version: 0.10.0
+ * Version: 0.11.0-beta.1
  * Description: A WSU HRS WordPress plugin to connect to and query external Microsoft SQL Server databases.
  * Author: Adam Turner, washingtonstateuniversity
  * Author URI: https://hrs.wsu.edu/
@@ -13,38 +13,78 @@
  * Requires PHP: 7.0
  *
  * @package HRSWP_Sqlsrv_DB
- * @since 0.1.0
  */
 
 namespace HRSWP\SQLSRV;
 
-// If this file is called directly, abort.
-if ( ! defined( 'WPINC' ) ) {
-	die;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
 }
 
 /**
- * Loads the core plugin Setup class.
+ * Activates the plugin.
  *
- * @since 0.1.0
+ * @since 0.11.0
  */
-require __DIR__ . '/includes/class-setup.php';
+register_activation_hook(
+	__FILE__,
+	function(): void {
+		// Delete legacy option.
+		delete_option( 'hrswp_sqlsrv_db_plugin-status' );
+	}
+);
 
-// Starts things up.
-add_action( 'plugins_loaded', __NAMESPACE__ . '\load' );
-
-register_activation_hook( __FILE__, array( __NAMESPACE__ . '\Setup\Setup', 'activate' ) );
-register_uninstall_hook( __FILE__, array( __NAMESPACE__ . '\Setup\Setup', 'uninstall' ) );
+// Load plugin classes.
+require_once dirname( __FILE__ ) . '/includes/classes/class-sqlsrv-db.php';
+require_once dirname( __FILE__ ) . '/includes/classes/class-sqlsrv-query.php';
 
 /**
- * Creates an instance of the Setup class.
+ * Uninstalls the plugin.
  *
- * @since 0.1.0
- *
- * @return Setup An instance of the Setup class.
+ * @since 0.11.0
  */
-function load() {
-	$hrswp_sqlsrv_db = Setup\Setup::get_instance( __FILE__ );
+function uninstall(): void {
+	if ( ! current_user_can( 'activate_plugins' ) ) {
+		return;
+	}
 
-	return $hrswp_sqlsrv_db;
+	// Delete plugin options.
+	delete_option( 'hrswp_sqlsrv_db_plugin-status' );
 }
+register_uninstall_hook( __FILE__, __NAMESPACE__ . '\uninstall' );
+
+/**
+ * Verifies plugin dependencies.
+ *
+ * @since 0.11.0
+ *
+ * @return bool True if dependencies are met, false if not.
+ */
+function verify_plugin_dependencies(): bool {
+	// Check for the HRSWP Sqlsrv configuration file.
+	if (
+		! file_exists( ABSPATH . 'hrswp-sqlsrv-config.php' ) &&
+		! file_exists( dirname( ABSPATH ) . '/hrswp-sqlsrv-config.php' )
+	) {
+		return false;
+	}
+
+	return true;
+}
+
+add_action(
+	'plugins_loaded',
+	function(): void {
+		if ( ! verify_plugin_dependencies() ) {
+			add_action(
+				'admin_notices',
+				function(): void {
+					printf(
+						'<div class="notice notice-error"><p>%s</p></div>',
+						esc_html__( 'ERROR: There doesn\'t seem to be a', 'hrswp-sqlsrv-db' ) . '<code>hrswp-sqlsrv-config.php</code>' . esc_html__( 'file. This is required for the HRSWP Sqlsrv DB plugin to work.', 'hrswp-sqlsrv-db' )
+					);
+				}
+			);
+		}
+	}
+);
